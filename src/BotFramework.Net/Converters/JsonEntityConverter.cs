979b7b0553ad53f54741dc7f8a44fdd6c0b3cceb
Alexey Strakh <alexey.strakh@microsoft.com>
2018-04-24 20:50:19 -0700

    update code-style for the BotFramework.Net project

--- src/BotFramework.Net/Converters/JsonEntityConverter.cs
+++ src/BotFramework.Net/Converters/JsonEntityConverter.cs
@@ -5,30 +5,35 @@ using Newtonsoft.Json.Linq;
 
 namespace BotFramework
 {
-	public class JsonEntityConverter : JsonCreationConverter<Entity>
-	{
-		public static Dictionary<string, Type> TypeMappings = new Dictionary<string, Type>
-		{
-			{Place.ContentType,typeof(Place)},
-			{GeoCoordinates.ContentType,typeof(GeoCoordinates)},
-			{Mention.ContentType,typeof(Mention)},
-		};
-		protected override Entity Create (System.Type objectType, JObject jsonObject, JsonReader reader)
-		{
-			string type = "";
-			try {
-				JToken token;
-				if (jsonObject.TryGetValue ("type", StringComparison.CurrentCultureIgnoreCase, out token)) {
-					type = token.ToString ();
-					Type objType = typeof (Entity);
-					TypeMappings.TryGetValue (type, out objType);
-					return (Entity)Activator.CreateInstance (objType);
-				}
-				return new Entity ();
-			} catch (Exception ex) {
-				Console.WriteLine (ex);
-			}
-			return new Entity ();
-		}
-	}
+    public class JsonEntityConverter : JsonCreationConverter<Entity>
+    {
+        public static Dictionary<string, Type> TypeMappings = new Dictionary<string, Type>
+        {
+            {Place.ContentType,typeof(Place)},
+            {GeoCoordinates.ContentType,typeof(GeoCoordinates)},
+            {Mention.ContentType,typeof(Mention)},
+        };
+
+        protected override Entity Create(System.Type objectType, JObject jsonObject, JsonReader reader)
+        {
+            string type = "";
+            try
+            {
+                JToken token;
+                if (jsonObject.TryGetValue("type", StringComparison.CurrentCultureIgnoreCase, out token))
+                {
+                    type = token.ToString();
+                    Type objType = typeof(Entity);
+                    TypeMappings.TryGetValue(type, out objType);
+                    return (Entity)Activator.CreateInstance(objType);
+                }
+                return new Entity();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            return new Entity();
+        }
+    }
 }
